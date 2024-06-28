55a4ffdc2d84662ee3927615e254647e35004ff5
Dor Katzir <katzirdor@gmail.com>
2024-06-28 18:16:30 +0300

    updating a movie with response

--- update_movie.php
+++ update_movie.php
@@ -9,13 +9,30 @@
     // what can be updated? box_office, stars, storyline
 
     if( isset($_POST['id']) && isset($_POST['storyline']) && isset($_POST['stars'])&& isset($_POST['box_office']) ){
-        // success
+        // move on and update movie
+        $id = $_POST['id'];
+        $storyline = $_POST['storyline'];
+        $stars = $_POST['stars'];
+        $box_office = $_POST['box_office'];
 
+        $stmt = $con->prepare("UPDATE movies SET storyline='$storyline', stars='$stars', box_office='$box_office' 
+                                WHERE id='$id ");
+                if( $stmp->execute() ){
+                    // success
+                    $response['error'] = false;
+                    $response['message'] = 'Movie Updated Successfully';
+                } else {
+                    // fail
+                    $response['error'] = true;
+                    $response['message'] = 'Movie Failed to Update';
+                }
+                
     } else {
-        // fail
-
+        // we dont have info to update the movie
+        $response['error'] = true;
+        $response['message'] = 'Please provide: id, storyline, stars and box_office';
     }
 
-
+    echo json_encode($response);
 
 ?>
